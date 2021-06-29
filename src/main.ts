49b8f530c115739aa60d330cf79d6293562989a2
konriz <konriz@gmail.com>
2021-06-29 13:58:37 +0200

    CORS allowed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const originFunction = (origin: any, callback: any) => {
+    callback(null, true);
+  };
+  const corsOptions = {
+    origin: originFunction
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
+
 bootstrap();
