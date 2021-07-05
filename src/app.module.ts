cc8f87357da4017a89ca38961484d29c48ad3033
konriz <konriz@gmail.com>
2021-07-05 13:06:06 +0200

    Add .env config for mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { TipJar, TipJarSchema } from "./schema/tip-jar.schema";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://mongo/nest"),
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URL || "mongodb://localhost/nest"),
     MongooseModule.forFeature([{name: TipJar.name, schema: TipJarSchema}])],
   controllers: [AppController],
   providers: [AppService]
